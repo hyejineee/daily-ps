1921146374cb9dcccc412db21f79cd3f7f2afd0e
hyejineee <dev.hyejin04@gmail.com>
2022-06-13 16:31:25 +0900

    프로그래머스 - 제일 작은 수 제거하기

---
+++ 2022/0613/deleteMin.test.js
@@ -0,0 +1,9 @@
+const deleteMin = (arr)=>{
+    return arr.length <=1? [-1] : arr.filter(v => v > Math.min(...arr)) 
+}
+
+test(`제일 작은 수 제거`, ()=>{
+    expect(deleteMin([4,3,1,2])).toEqual([4,3,2])
+    expect(deleteMin([10])).toEqual([-1])
+    
+})
