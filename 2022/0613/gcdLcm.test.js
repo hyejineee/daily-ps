059cc8ec8eb9911014810f8e2c36d04d21f6e02c
hyejineee <dev.hyejin04@gmail.com>
2022-06-13 16:22:39 +0900

    프로그래머스 - 최대공약수, 최소공배수 구하기

---
+++ 2022/0613/gcdLcm.test.js
@@ -0,0 +1,16 @@
+const solution = (n1,n2)=>{
+    const g = gcd(n1,n2)
+    return [g, n1 * n2 /g]
+}
+
+const gcd = (n1, n2)=>{
+    if(n2 == 0) return n1
+    else return gcd(n2, n1%n2)
+}
+
+
+
+test(`최대 공약수, 최소 공배수 구하기`, ()=>{
+    expect(solution(3,12)).toEqual([3,12])
+    expect(solution(2,5)).toEqual([1,10])
+})
