78f3f81f6002b3906f52a6c6b42ee17ef17beb7e
hyejineee <dev.hyejin04@gmail.com>
2022-06-15 11:46:54 +0900

    프로그래머스 - 소수찾기

---
+++ 2022/0615/findPrime.test.js
@@ -0,0 +1,19 @@
+const solution = (n) => {
+    const primes = new Array(n + 1).fill(true)
+    primes[0] = false
+    primes[1] = false
+    primes[2] = true
+
+    for (let i = 2; i <= n; i++) {
+        console.log(primes[i])
+        if (primes[i] == false) continue
+        for (let j = 2 * i; j <= n; j += i) primes[j] = false
+    }
+
+    return primes.filter((v) => v).length
+}
+
+test(`소수찾기`, () => {
+    expect(solution(10)).toEqual(4)
+    expect(solution(5)).toEqual(3)
+})
