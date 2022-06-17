2c4b50c40bdcde556063ff5489f2262577498b9b
hyejineee <dev.hyejin04@gmail.com>
2022-06-17 15:35:02 +0900

    프로그래머스 - 문자열 다루기 기본

---
+++ 2022/0617/stringBasic.test.js
@@ -0,0 +1,18 @@
+const solution = (s) => {
+    if(!(s.length === 4 || s.length === 6)) return false
+   
+    console.log(s.split(''))
+    for(let i of s.split('')){
+        console.log(i)
+        if(isNaN(parseInt(i))) return false
+        if(parseInt(i) < 0) return false
+    }
+
+    return true
+}
+
+test(`문자열 다루기 기본`, () => {
+    expect(solution("1234")).toEqual(true)
+    expect(solution("a234")).toEqual(false)
+    expect(solution("1e22")).toEqual(false)
+})
