7e7ad9e1e6a471e79153cbb9472492efb1110cb3
hyejineee <dev.hyejin04@gmail.com>
2022-06-17 15:56:22 +0900

    프로그래머스 - 문자열 내 p와 y의 개수

---
+++ 2022/0617/countOfPY.test.js
@@ -0,0 +1,14 @@
+const solution = (s) => {
+    const result = s.toLowerCase().split('').reduce((acc, cur) => {
+        if (cur === 'p') acc[0]++
+        if (cur === 'y') acc[1]++
+        return acc
+    }, [0,0])
+
+    return result[0] ===result[1]
+}
+
+test(`문자열 내 p와 y의 개수`, () => {
+    expect(solution("pPoooyY")).toEqual(true)
+    expect(solution("Pyy")).toEqual(false)
+})
