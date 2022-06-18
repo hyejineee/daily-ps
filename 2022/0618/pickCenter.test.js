4287fa736e12789b1c2c0c5eeef85b77a0b1a070
hyejineee <dev.hyejin04@gmail.com>
2022-06-18 16:39:50 +0900

    프로그래머스 - 비밀지도

--- 2022/0618/pickCenter.test.js
+++ 2022/0618/pickCenter.test.js
@@ -1,15 +1,8 @@
 const solution = (s) => {
-   
-    return s.length % 2 !== 0 ? s[parseInt(s.length/2)] : s[parseInt(s.length/2) -1] + s[parseInt(s.length/2)]
+    return s.length % 2 !== 0 ? s[parseInt(s.length / 2)] : s[parseInt(s.length / 2) - 1] + s[parseInt(s.length / 2)]
 }
 
 test(`가운데 글자 가져오기`, () => {
     expect(solution("abcde")).toEqual("c")
     expect(solution("qwer")).toEqual("we")
 })
-
-
-// function solution(arr)
-// {
-//     return arr.filter((val,index) => val != arr[index+1]);
-// }
---
+++ 2022/0618/secretMap.test.js
@@ -0,0 +1,15 @@
+const solution = (n, arr1, arr2) => {
+
+    const result = []
+    for (let i = 0; i < n; i++) {
+        result.push(("0".repeat(16)+(arr1[i] | arr2[i]).toString(2)).substr(-n))
+    }
+
+    const r = result.map((v) => v.split('').map((v2)=> v2 == 0 ? ' ' : '#' ).join(''))
+    return r 
+}
+
+test(`비밀지도`, () => {
+    expect(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])).toEqual(["#####", "# # #", "### #", "#  ##", "#####"])
+    expect(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10])).toEqual(["######", "###  #", "##  ##", " #### ", " #####", "### # "])
+})
