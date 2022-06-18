b10612ee6e5ab881e1d183057b9cacfc6d3680e0
hyejineee <dev.hyejin04@gmail.com>
2022-06-18 15:50:26 +0900

    프로그래머스 - 가운데 글자 가져오기

--- 2022/0618/sameNumber.test.js
+++ 2022/0618/sameNumber.test.js
@@ -1,32 +1,17 @@
-const solution = (dartResult) => {
+const solution = (arr) => {
     const result = []
-
-    const number = dartResult.split(/S|D|T|\#|\*/g).filter((e) => !isNaN(parseInt(e))).map((v) => parseInt(v))
-    const op = dartResult.split('').filter((e) => isNaN(parseInt(e)))
-
-    for (let i of op) {
-        if (i === 'S') result.push(number.shift())
-        if (i === 'D') result.push(number.shift() ** 2)
-        if (i === 'T') result.push(number.shift() ** 3)
-
-        if(i === '*'){
-            result[result.length -1] *=2
-            result[result.length -2] *=2
+    arr.forEach(element => {
+        if (result.at(-1) !== element) {
+            result.push(element)
         }
+    });
 
-        if(i === '#'){
-            result[result.length -1] *=-1
-        }
-    }
-
-    return result.reduce((acc, cur) => acc + cur, 0)
-
+    return result
 }
 
-test(`다트게임`, () => {
-    expect(solution("1D2S#10S")).toEqual(9)
-    expect(solution("1S*2T*3S")).toEqual(23)
-    expect(solution("1D#2S*3S")).toEqual(5)
+test(`같은 숫자는 싫어`, () => {
+    expect(solution([1, 1, 3, 3, 0, 1, 1])).toEqual([1, 3, 0, 1])
+    expect(solution([4, 4, 4, 3, 3])).toEqual([4, 3])
 })
 
 
