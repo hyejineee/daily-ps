a2cf15e109d9ec018624f63e21e33fe9ebb2c32c
hyejineee <dev.hyejin04@gmail.com>
2022-06-18 14:16:46 +0900

    프로그래머스 - 같은 숫자는 싫어

---
+++ 2022/0618/sameNumber.test.js
@@ -0,0 +1,15 @@
+const solution = (arr) => {
+    const result = []
+    arr.forEach(element => {
+        if(result.at(-1) !== element){
+            result.push(element)
+        }
+    });
+
+    return result
+}
+
+test(`같은 숫자는 싫어`, () => {
+    expect(solution([1,1,3,3,0,1,1]	)).toEqual([1,3,0,1])
+    expect(solution([4,4,4,3,3])).toEqual([4,3])
+})
