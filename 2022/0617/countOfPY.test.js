2d5f5c476074c38aa7141293633bf012018a93cc
hyejineee <dev.hyejin04@gmail.com>
2022-06-18 16:46:38 +0900

    프로그래머스 - 나누어 떨어지는 숫자 배열

--- 2022/0617/countOfPY.test.js
+++ 2022/0617/countOfPY.test.js
@@ -3,9 +3,9 @@ const solution = (s) => {
         if (cur === 'p') acc[0]++
         if (cur === 'y') acc[1]++
         return acc
-    }, [0,0])
+    }, [0, 0])
 
-    return result[0] ===result[1]
+    return result[0] === result[1]
 }
 
 test(`문자열 내 p와 y의 개수`, () => {
---
+++ 2022/0618/divisibleArray.test.js
@@ -0,0 +1,10 @@
+const solution = (arr, divisor) => {
+    const filter = arr.filter((e) => e %divisor ==0)
+    return filter.length == 0 ? [-1] : filter.sort((a,b) => a-b)
+}
+
+test(`나누어 떨어지는 숫자배열`, () => {
+    expect(solution([5, 9, 7, 10]	, 5)).toEqual([5, 10])
+    expect(solution([2, 36, 1, 3]	, 1)).toEqual([1, 2, 3, 36]	)
+    expect(solution([3,2,6]	, 10)).toEqual([-1]	)
+})
