89b739a1adfca6d0eed0eadee622b8e7659cd4c2
hyejineee <dev.hyejin04@gmail.com>
2022-07-06 11:26:52 +0900

    프로그래머스 - k번쨰 수

---
+++ 2022/0705/kthNumber.test.js
@@ -0,0 +1,18 @@
+const solution = (array, commands) => {
+  return commands.map(
+    ([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
+  );
+};
+
+test(`k번째 수`, () => {
+  expect(
+    solution(
+      [1, 5, 2, 6, 3, 7, 4],
+      [
+        [2, 5, 3],
+        [4, 4, 1],
+        [1, 7, 3],
+      ]
+    )
+  ).toEqual([5, 6, 3]);
+});
