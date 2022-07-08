3c579d4dfbb1aebeb0cb9a9a12ad7f6bd4ed4ac0
hyejineee <dev.hyejin04@gmail.com>
2022-07-08 09:17:46 +0900

    프로그래머스 - 없는 숫자 더하기

--- 2022/0708/addNumber.test.js
+++ 2022/0708/addNumber.test.js
@@ -9,3 +9,9 @@ test(`없는 숫자 더하기`, () => {
   expect(solution([1, 2, 3, 4, 6, 7, 8, 0], [true, false, true])).toEqual(14);
   expect(solution([5, 8, 4, 0, 6, 7, 9])).toEqual(6);
 });
+
+
+// 전체 합을 이용한 풀이
+// function solution(numbers) {
+//   return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+// }
