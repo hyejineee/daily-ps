4daaaae4127ceb16030a3254cd29b1a2389cd8c7
hyejineee <dev.hyejin04@gmail.com>
2022-07-11 18:34:44 +0900

    프로그래머스 - 숫자 문자열과 영단어

---
+++ 2022/0711/numberStrAndEglishWord.test.js
@@ -0,0 +1,30 @@
+const solution = (s) => {
+  const numberWordMap = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  let result = s;
+  Object.entries(numberWordMap).forEach(([k, v]) => {
+    const reg = new RegExp(k, "g");
+    result = result.replace(reg, numberWordMap[k]);
+  });
+
+  return Number(result);
+};
+
+test(`숫자 문자열과 영단어`, () => {
+  expect(solution("one4seveneight")).toEqual(1478);
+  expect(solution("23four5six7")).toEqual(234567);
+  expect(solution("2three45sixseven")).toEqual(234567);
+  expect(solution("123")).toEqual(123);
+  expect(solution("1oneoneone")).toEqual(1111);
+});
