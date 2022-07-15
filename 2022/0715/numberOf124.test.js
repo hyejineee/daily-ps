77706ac446ef380fdc7f8e0b07abf74fb67e5100
hyejineee <dev.hyejin04@gmail.com>
2022-07-15 10:07:33 +0900

    124 나라의 숫자

---
+++ 2022/0715/numberOf124.test.js
@@ -0,0 +1,26 @@
+const solution = (n) => {
+  return get124(n, "").split("").reverse().join("");
+};
+
+const get124 = (q, result) => {
+  if (q === 0) {
+    return result;
+  }
+
+  const nq = Math.floor(q / 3);
+
+  return get124(q % 3 === 0 ? nq - 1 : nq, result + [4, 1, 2][q % 3]);
+};
+
+test(`124 나라의 숫자`, () => {
+  expect(solution(13)).toEqual("111");
+  expect(solution(12)).toEqual("44");
+  expect(solution(11)).toEqual("42");
+  expect(solution(10)).toEqual("41");
+});
+
+// function change124(n) {
+//   return n === 0
+//     ? ""
+//     : change124(parseInt((n - 1) / 3)) + [1, 2, 4][(n - 1) % 3];
+// }
