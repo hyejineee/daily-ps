b46334751271601e88c8d211c99754173eb4717a
hyejineee <dev.hyejin04@gmail.com>
2022-10-13 11:58:45 +0900

    프로그래머스 - 점프와 순간이동

--- 2022/1013/점프와 순간 이동.test.js
+++ 2022/1013/점프와 순간 이동.test.js
@@ -12,6 +12,12 @@ const solution = (n) => {
   return count;
 };
 
+// 이진수의 1의 개수로 풀이
+function solution(n) {
+  return n.toString(2).replace(/0/g, "").length;
+}
+
+
 test(`점프와 순간 이동`, () => {
   expect(solution(5)).toEqual(2);
   expect(solution(6)).toEqual(2);
