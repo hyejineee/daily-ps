71895d8697ac8cfccca3d25b9c161ddab12934bc
hyejineee <dev.hyejin04@gmail.com>
2022-11-04 17:52:30 +0900

    프로그래머스 - 두 큐 합 같게 만들기

---
+++ 2022/1104/두 큐 합 같게 만들기.test.js
@@ -0,0 +1,56 @@
+const solution = (queue1, queue2) => {
+  const total = sum(queue1) + sum(queue2);
+
+  if (total % 2 !== 0) return -1;
+
+  const half = Number(total / 2);
+
+  if (
+    queue1.filter((e) => e > half).length > 0 ||
+    queue2.filter((e) => e > half).length > 0
+  )
+    return -1;
+
+  let count = 0;
+  let sum1 = sum(queue1);
+  let sum2 = sum(queue2);
+
+  const merge = [...queue1, ...queue2];
+  let s1 = 0,
+    s2 = queue1.length;
+
+  while (sum1 !== half) {
+    if (s1 > queue1.length + 2 || s2 > merge.length + 2) return -1;
+
+    if (sum1 > sum2) {
+      const p1 = merge[s1];
+
+      sum1 -= p1;
+      sum2 += p1;
+
+      s1++;
+    } else {
+      const p2 = merge[s2];
+
+      sum1 += p2;
+      sum2 -= p2;
+
+      s2++;
+    }
+
+    count++;
+  }
+
+  return count;
+};
+
+const sum = (arr) => {
+  return arr.reduce((acc, cur) => acc + cur, 0);
+};
+
+test(`두 큐 합 같게 만들기`, () => {
+  expect(solution([3, 2, 7, 2], [4, 6, 5, 1])).toEqual(2);
+  expect(solution([1, 2, 1, 2], [1, 10, 1, 2])).toEqual(7);
+  expect(solution([1, 1], [1, 5])).toEqual(-1);
+  expect(solution([1, 1, 1], [5, 1, 1])).toEqual(4);
+});
