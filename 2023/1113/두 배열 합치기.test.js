5072efde09e134aeadaead623c0d194fea2b55c6
hyejineee <dev.hyejin04@gmail.com>
2023-11-14 10:01:03 +0900

    231113 두 배열 합치기

---
+++ 2023/1113/두 배열 합치기.test.js
@@ -0,0 +1,44 @@
+const solution = (arr1, arr2) => {
+  let p1 = 0;
+  let p2 = 0;
+
+  const result = [];
+
+  while (true) {
+    if (p1 >= arr1.length || p2 >= arr2.length) break;
+
+    const v1 = arr1[p1];
+    const v2 = arr2[p2];
+
+    if (v1 > v2) {
+      result.push(v2);
+      p2++;
+      continue;
+    }
+
+    result.push(v1);
+    p1++;
+  }
+
+  while (p1 < arr1.length) {
+    result.push(arr1[p1]);
+    p1++;
+  }
+
+  while (p2 < arr2.length) {
+    result.push(arr2[p2]);
+    p2++;
+  }
+
+  return result;
+};
+
+test(`두 배열 합치기`, () => {
+  expect(solution([1, 3, 5], [2, 3, 6, 7, 9])).toEqual([
+    1, 2, 3, 3, 5, 6, 7, 9,
+  ]);
+
+  expect(solution([2, 3, 6, 7, 9], [1, 3, 5])).toEqual([
+    1, 2, 3, 3, 5, 6, 7, 9,
+  ]);
+});
