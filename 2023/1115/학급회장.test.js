df1eaddf35d3eb2791ba061ec836ea92c8d3a6ab
hyejineee <dev.hyejin04@gmail.com>
2023-11-15 22:41:32 +0900

    231115 학급회장

---
+++ 2023/1115/학급회장.test.js
@@ -0,0 +1,18 @@
+const solution = (candidates) => {
+  return [...candidates].reduce(
+    (acc, cur) => {
+      acc[0].hasOwnProperty(cur) ? (acc[0][cur] += 1) : (acc[0][cur] = 0);
+
+      if (acc[1] < acc[0][cur]) {
+        acc[1] = acc[0][cur];
+        acc[2] = cur;
+      }
+      return acc;
+    },
+    [{}, 0, ""]
+  )[2];
+};
+
+test(`학급회장`, () => {
+  expect(solution("BACBACCACCBDEDE")).toEqual("C");
+});
